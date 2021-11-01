23e31aa43aec735a20e0640e36b7564ad066958e
Ihtiandiko03 <74673480+Ihtiandiko03@users.noreply.github.com>
2021-11-01 20:52:04 +0700

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2021 at 02:17 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `minggu4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mahasiswa`
+--
+
+CREATE TABLE `tbl_mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama_mahasiswa` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `jurusan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_mahasiswa`
+--
+
+INSERT INTO `tbl_mahasiswa` (`id`, `nama_mahasiswa`, `alamat`, `jenis_kelamin`, `tgl_masuk`, `jurusan`) VALUES
+(11, 'Heksa', 'Banjarrejo', 'Laki-laki', '2021-10-31', 'Ilmu Tanah'),
+(13, 'Iva', 'Teluk', 'Perempuan', '2021-10-28', 'Teknik Elektro'),
+(14, 'Pakih', 'Bandar Lampung', 'Laki-laki', '2021-10-12', 'Kimia'),
+(15, 'Intan', 'Belakang Wisma', 'Perempuan', '2021-11-01', 'Kesehatan Masyarakat'),
+(16, 'Ihtiandiko', 'Indralaya', 'Laki-laki', '2021-11-01', 'Kimia');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_mahasiswa`
+--
+ALTER TABLE `tbl_mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_mahasiswa`
+--
+ALTER TABLE `tbl_mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
