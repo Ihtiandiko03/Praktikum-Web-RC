cd2a5a13e169bcf73e31e94ec7ed968523b1e2ba
Ihtiandiko03 <74673480+Ihtiandiko03@users.noreply.github.com>
2021-11-15 21:37:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
         <title>Form</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        
-        <!-- <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
-    
+   
         <link href="style.css" rel="stylesheet" media="all"> 
     </head>
     <body id="body">
